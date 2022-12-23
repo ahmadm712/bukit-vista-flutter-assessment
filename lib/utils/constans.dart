9137900699ed0728fcd8c97956784bedfe8a496d
ahmadm712 <ahmadmuji2808@gmail.com>
2022-12-24 02:40:42 +0700

    add some constants

--- lib/utils/constans.dart
+++ lib/utils/constans.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 const String assetName = 'assets/json/';
 
 // HardCode Information
@@ -6,3 +8,8 @@ String infoStartDate = 'START DATE';
 String infoEndDate = 'END DATE';
 String starDate = '10 Nov 2020';
 String endDate = '16 Nov 2020';
+
+const List<Tab> tabs = [
+  Tab(text: 'Guest Journey'),
+  Tab(text: 'Reservation'),
+];
