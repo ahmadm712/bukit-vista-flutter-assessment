b37cfa7b56addba29a0841b09333ad27f9ab2a27
ahmadm712 <ahmadmuji2808@gmail.com>
2022-12-24 00:44:43 +0700

    add some constants String

---
+++ lib/utils/constans.dart
@@ -0,0 +1,8 @@
+const String assetName = 'assets/json/';
+
+// HardCode Information
+/// HOME
+String infoStartDate = 'START DATE';
+String infoEndDate = 'END DATE';
+String starDate = '10 Nov 2020';
+String endDate = '16 Nov 2020';
