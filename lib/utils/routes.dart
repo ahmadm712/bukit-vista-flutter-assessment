adbd9c2517a14c5a7af6daf7d95650565c8b0278
ahmadm712 <ahmadmuji2808@gmail.com>
2022-12-24 02:40:55 +0700

    add routes to detail booking

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:test_bukit_vista/data/models/bookings_models.dart';
 import 'package:test_bukit_vista/ui/pages/detail_booking.dart';
 
 var mainRoutes = (RouteSettings settings) {
   switch (settings.name) {
     case DetailBookingPage.routeName:
+      var data = settings.arguments as Booking;
       return MaterialPageRoute(
-        builder: (context) => const DetailBookingPage(),
+        builder: (context) => DetailBookingPage(booking: data),
       );
     default:
       return MaterialPageRoute(
