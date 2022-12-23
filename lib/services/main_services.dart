9b3e9af495f038a1cc918aec6eb5fcc81d3355d5
ahmadm712 <ahmadmuji2808@gmail.com>
2022-12-24 02:55:33 +0700

    create currency to idr

--- lib/services/main_services.dart
+++ lib/services/main_services.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class MainServices {
   Future<String> readJson(String path) async {
     String response = await rootBundle.loadString(path);
     return response;
+  }
+
+  String convertToIdr(dynamic number, int decimalDigit) {
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'Rp ',
+      decimalDigits: decimalDigit,
+    );
+    return currencyFormatter.format(number);
   }
 }
